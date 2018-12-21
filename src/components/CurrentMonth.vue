ec28dfd3ffe4a4bf689f2e88062b79441f15152c
Paulo Filho <pcf.feat@gmail.com>
2018-12-21 10:14:28 -0300

    Fleshing out month selector; separating vuex store to a separate file

---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div>{{ formattedDate }}</div>
+  <button @click="dec">-</button>
+  <button @click="inc">+</button>
+</div>
+</template>
+<script>
+export default {
+  methods: {
+    dec() {
+      console.log('dec');
+    },
+    inc() {
+      console.log('inc');
+    }
+  },
+  computed: {
+    month() {
+      return this.$store.state.currentMonth;
+    },
+    year (){
+      return this.$store.state.currentYear;
+    },
+    formattedDate() {
+      return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM YYYY');
+    }
+  }
+}
+</script>
