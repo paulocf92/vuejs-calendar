5feb10a0d02f7bda83e4440619859b1ac58bbaeb
Paulo Filho <pcf.feat@gmail.com>
2018-12-27 16:25:02 -0300

    Adding focus and dispatching event creation through keypress

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,7 +2,7 @@
   <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
     <h4>Add an event</h4>
     <div class="text">
-      <input type="text" v-model="description" placeholder="Dinner at Pancho's">
+      <input v-focus type="text" v-model="description" placeholder="Dinner at Pancho's" @keyup.enter="create">
     </div>
     <div class="buttons">
       <button @click="create">Create</button>
@@ -38,6 +38,13 @@ export default {
     },
     left() {
       return `${this.$store.state.eventFormPosX}px`;
+    }
+  },
+  directives: {
+    focus: {
+      update(el) {
+        el.focus();
+      }
     }
   }
 }
