c5d4753a2aa5f0f2abf682afb860860503725cdd
Paulo Filho <pcf.feat@gmail.com>
2018-12-27 15:23:54 -0300

    QOL for EventForm

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -2,7 +2,7 @@
   <div id="event-form" :class="{ active: active }" :style="{ top: top, left: left }">
     <h4>Add an event</h4>
     <div class="text">
-      <input type="text" v-model="description">
+      <input type="text" v-model="description" placeholder="Dinner at Pancho's">
     </div>
     <div class="buttons">
       <button @click="create">Create</button>
@@ -22,7 +22,11 @@ export default {
       this.$store.commit('eventFormActive', false);
     },
     create() {
-      this.$store.commit('addEvent', this.description);
+      if (this.description.length > 0) {
+        this.$store.commit('addEvent', this.description);
+        this.description = '';
+        this.$store.commit('eventFormActive', false);
+      }
     }
   },
   computed: {
