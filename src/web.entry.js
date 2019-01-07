4f3ddf1cd59cfdb159030479e4b2d97559734df3
Paulo Filho <pcf.feat@gmail.com>
2019-01-07 14:50:01 -0300

    Moving mock data into HTML template

--- src/web.entry.js
+++ src/web.entry.js
@@ -9,12 +9,20 @@ Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.mome
 
 import App from './components/App.vue';
 
-let events = [
+/*let events = [
   { description: 'Christmas', date: moment('2018-12-25', 'YYYY-MM-DD') },
   { description: 'New Year', date: moment('2019-01-01', 'YYYY-MM-DD') },
   { description: 'City Aniversary', date: moment('2018-12-20', 'YYYY-MM-DD') }
-];
-let initialState = Object.assign({}, store.state, {events});
+];*/
+
+let events = window.__INITIAL_STATE__.map(event => { // Reconstruct event dates to make them moment objects
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+});
+
+let initialState = Object.assign({}, store.state, {events}); // Copy state with new events property into an empty object
 store.replaceState(initialState);
 
 new Vue({
