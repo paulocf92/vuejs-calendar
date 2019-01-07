959bd1037ee7a44cbd8d384fbf76de4cd6deb121
Paulo Filho <pcf.feat@gmail.com>
2019-01-07 15:08:49 -0300

    Moving initial state from raw html file into server, then serializing events array and appending it back to html; management done by a global var

--- server.js
+++ server.js
@@ -5,16 +5,24 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+let events = [
+  { description: 'Christmas', date: moment('2018-12-25', 'YYYY-MM-DD') },
+  { description: 'New Year', date: moment('2019-01-01', 'YYYY-MM-DD') },
+  { description: 'City Aniversary', date: moment('2018-12-20', 'YYYY-MM-DD') }
+];
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--APP-->';
+  res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>`));
 
 });
-
-let events = [];
 
 app.use(require('body-parser').json()); // Converts incoming req into json
 app.post('/add_event', (req, res) => {
